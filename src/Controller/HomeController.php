ba0e7c5632f8050ee0a02f19fc28de78d18a3431
MohamedDridii <mouhamed.eldridi@gmail.com>
2025-07-10 14:12:13 +0100

    injecting managerRegistry or entityREpository in default controller and get data from the database in the home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\CityRepository;
+use App\Repository\CountryRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,22 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(CityRepository $city_rep,CountryRepository $country_rep ): Response
     {
+        dd($city_rep->findAll(),$country_rep->findAll());
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
+    // or we can use manage registry to replace injecting each repository seperatly
+    /*#[Route('/home', name: 'app_home')]
+    public function index1(ManagerRegistry $doctrine ): Response
+    {
+        $city_rep=$doctrine->getRepository(CityRepository::class);
+        $country_rep=$doctrine->getRepository(CountryRepository::class);
+        dd($city_rep->findAll(),$country_rep->findAll());
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }*/
 }
