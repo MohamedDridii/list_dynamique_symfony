e2d8ecce4f842405dc53f548d727c4d3d3d418bb
MohamedDridii <mouhamed.eldridi@gmail.com>
2025-07-11 09:50:47 +0100

    handling request for forms

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,9 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 final class HomeController extends AbstractController
 {
@@ -36,15 +38,18 @@ final class HomeController extends AbstractController
         ]);
     }*/
      #[Route('/form', name: 'app_form')]
-    public function createfrom( ): Response
+    public function createfrom( Request $request): Response
     {
         $form=$this->createFormBuilder()
-        ->add('name',TextType::class)//la methode add ajoute des champs au formulaire
+        ->add('name',TextType::class,[
+            'constraints'=>new NotBlank(['message'=>'Please enter your name '])// make the field non empty and add a message if empty
+        ])//la methode add ajoute des champs au formulaire
         ->add('country',EntityType::class,[
             'class'=>Country::class,
             'placeholder'=>'please choose a country',
             'choice_label'=>'name',//indique le champ dans l'entity country a utilise comme label c'est une option de la classe Entity type 
-            'query_builder'=> fn(CountryRepository $country)=>$country->createQueryBuilder('c')->orderBy('c.Name',"ASC") // fait l'ordre des pays par ordre croisssant en utilisant querybuilder 
+            'query_builder'=> fn(CountryRepository $country)=>$country->createQueryBuilder('c')->orderBy('c.Name',"ASC"), // fait l'ordre des pays par ordre croisssant en utilisant querybuilder 
+            'constraints'=> new NotBlank(['message'=>'please choose a country'])
         ])
         ->add('city',EntityType::class,[
             'class'=>City::class,
@@ -52,11 +57,22 @@ final class HomeController extends AbstractController
             'placeholder'=>'please choose a city',
             'query_builder'=> function(CityRepository $city){//une autre facon d'ecrire la meme fonction
                 return $city->createQueryBuilder('c')->orderBy('c.Name',"ASC") ;
-            }
+            },
+            'constraints'=> new NotBlank(['message'=>'please choose a city'])
         ])
-        ->add('message',TextareaType::class)
-        ->getForm();//pour reupere le formulaire
-        // dd($form->createView());
+        ->add('message',TextareaType::class,[
+            'constraints'=> new NotBlank(['message'=>'seems like you have no problems'])
+        ])
+        ->getForm();//pour recupere le formulaire
+
+
+        //dd($form->get('city')->getName()); chaque formulaire et composer des champs et ces champ sont aussi des formualeire donc en peut acceder au formlaire fils avec cette methode 
+        //dd($form->getConfig());
+
+        $form->handleRequest($request);//recupere les donne dans le formulaire 
+         if ($form->isSubmitted()&& $form->isValid()){
+             dd($form->getData());// ce ligne permet de recuperer data de formulaire 
+         }
         return $this->render('home/index.html.twig', [
             'form' => $form->createView(),//creation de view de formulaire avec html simple ou bien utiliser renderForme et passeer seulement le variable $form sans la methode createView()
 
